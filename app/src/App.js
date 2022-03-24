bf5b24a6a51071fc2e3037ae4f5a3560acc88fd8
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2022-03-23 21:51:52 -0400

    first commit - npm i and rect app

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="title">Tracey McTague</h1>
+     <div></div>
+    </div>
+  );
+}
+
+export default App;
